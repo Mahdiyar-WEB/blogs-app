11660df86e7124e35369a44018a37e076d4de99e
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-05-31 01:11:13 +0330

    develop not-found page

---
+++ app/not-found.jsx
@@ -0,0 +1,21 @@
+"use client";
+import useMoveBack from "@/hooks/useMoveBack";
+import React from "react";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+
+const NotFound = () => {
+  const moveBack = useMoveBack();
+  return (
+    <main className="my-20">
+      <h1 className="text-center text-2xl md:text-3xl font-bold text-secondary-800">
+        صفحه ای که دنبالش بودی پیدا نشد! 😢
+      </h1>
+      <button className="flex text-primary-700 font-medium mx-auto justify-center items-center px-3 py-2 border border-primary-400 rounded-xl gap-2 mt-10" onClick={moveBack}>
+        <ArrowRightIcon className="size-6" />
+        <span>بازگشت به صفحه قبل</span>
+      </button>
+    </main>
+  );
+};
+
+export default NotFound;
