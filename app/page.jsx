b2e4b04bd5940cd7f4a715442296b84c15ce7dfc
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-05-31 00:28:19 +0330

    develop main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,29 @@
+import Button from "@/components/Button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="border bg-primary-400">salam</div>;
+  return (
+    <main className="my-20">
+      <h1 className="text-center text-2xl md:text-5xl font-bold text-secondary-800">
+        اپلیکیشن مدیریت بلاگ
+      </h1>
+      <p className="text-center mt-10 text-secondary-500 text-lg">
+        جایی که قراره بتونی یک اپلیکیشن بلاگ رو مدیریت کنی!
+        <br />
+        بتونی بلاگ بسازی - کامنت بگذاری و در پنلت همه اتفاقات رو رصد کنی!
+      </p>
+      <div className="flex justify-center gap-5 mt-10">
+        <Button variant="outline" className="!p-0">
+          <Link className="p-3" href="/blogs">
+            مطالعه بلاگ ها
+          </Link>
+        </Button>
+        <Button className="!p-0">
+          <Link className="p-3" href="/profile">
+            مدیریت بلاگ ها
+          </Link>
+        </Button>
+      </div>
+    </main>
+  );
 }
