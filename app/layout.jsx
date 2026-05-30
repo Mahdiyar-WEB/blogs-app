f017f7758817d83acf8b6a479a46de28ce9407dd
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-05-30 23:36:04 +0330

    add css classes to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
-import vazirFont from 'constants/localFont';
+import "@/styles/globals.css";
+import vazirFont from "constants/localFont";
 
 export const metadata = {
   title: "Create Next App",
@@ -9,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${vazirFont.variable} font-sans`}>{children}</body>
+      <body className={`${vazirFont.variable} font-sans min-h-screen`}>
+        {/* header */}
+        <div>header</div>
+        <div className="container xl:max-w-screen-xl">{children}</div>
+      </body>
     </html>
   );
 }
