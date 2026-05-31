24e56d82863e1309d12a0c4a6aa686bbe02aa9b1
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-01 01:12:31 +0330

    fetch posts from BE

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import callAPI from "app/utils/callAPI";
+import React from "react";
+import Post from "./_components/Post";
 
 export const metadata = {
-    title: 'بلاگ ها',
-  }
+  title: "بلاگ ها",
+};
 
-const Blogs = () => {
+const Blogs = async () => {
+  const res = await callAPI.get("post/list");
+  const {
+    data: { posts },
+  } = await res.json();
+  console.log("🚀 ~ Blogs ~ data:", posts);
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className="grid grid-cols-12 gap-3">
+      {posts.map((post,id) => {
+        return <Post key={id} {...post} />;
+      })}
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
