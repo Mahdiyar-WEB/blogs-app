ab41cfb67f1a789c01a819ea7ebd207ce57623ed
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-05-31 14:54:32 +0330

    update not-found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -6,8 +6,8 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 const NotFound = () => {
   const moveBack = useMoveBack();
   return (
-    <main className="my-20">
-      <h1 className="text-center text-2xl md:text-3xl font-bold text-secondary-800">
+    <main className="my-20 container">
+      <h1 className="text-center text-xl md:text-3xl font-bold text-secondary-800">
         صفحه ای که دنبالش بودی پیدا نشد! 😢
       </h1>
       <button className="flex text-primary-700 font-medium mx-auto justify-center items-center px-3 py-2 border border-primary-400 rounded-xl gap-2 mt-10" onClick={moveBack}>
