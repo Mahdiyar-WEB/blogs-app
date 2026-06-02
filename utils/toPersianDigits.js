503bf34e7438e4a458ceef550ab4dfa6259ad582
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-03 02:48:38 +0330

    add to persianDigits util

---
+++ utils/toPersianDigits.js
@@ -0,0 +1,10 @@
+const toPersianDigits = (n) => {
+  const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+  return n
+    .toString()
+    .split("")
+    .map((x) => farsiDigits[x])
+    .join("");
+};
+export default toPersianDigits;
