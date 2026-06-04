5d239cb506eb29db32ad1a78da3f7b16ecbfebe9
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-04 04:31:48 +0330

    add logging and cache component in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  cacheComponents:true,
   /* config options here */
   reactCompiler: true,
   images: {
-    remotePatterns: [new URL('http://localhost:5000/uploads/**')],
-    dangerouslyAllowLocalIP: true
+    remotePatterns: [new URL("http://localhost:5000/uploads/**")],
+    dangerouslyAllowLocalIP: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 };
 
