55a4105e6b82842b151c71507794e95038fefbd1
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-07 18:36:48 +0330

    update post fetch

--- services/callAPI.js
+++ services/callAPI.js
@@ -2,6 +2,26 @@ const callAPI = {
   get: async (endPoint) => {
     return await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${endPoint}`);
   },
+  post: async (endPoint, inputs) => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/${endPoint}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(inputs),
+        credentials: "include",
+      }
+    );
+    const data = await response.json();
+    if (!response.ok)
+      throw new Error(
+        data?.message ?? `HTTP error! status: ${response.status}`
+      );
+
+    return data;
+  },
 };
 
 export default callAPI;
