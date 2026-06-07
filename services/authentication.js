c212df1f8c2c01f0d57d6584c8b6d08f2cba0b90
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-07 19:48:32 +0330

    develop getUser in auth services

--- services/authentication.js
+++ services/authentication.js
@@ -7,6 +7,11 @@ const authentication = {
   signIn: async (inputs) => {
     return await callAPI.post("user/signin", inputs).then((data) => data);
   },
+  getUser: async () => {
+    const res = await callAPI.get("user/profile").then((data) => data);
+    const data = await res.json();
+    return data;
+  },
 };
 
 export default authentication;
