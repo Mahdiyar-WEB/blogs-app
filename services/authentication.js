15977bc068834ad64a842a5d94f8244139789d7c
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-07 18:37:01 +0330

    develop signUp service

---
+++ services/authentication.js
@@ -0,0 +1,9 @@
+import callAPI from "./callAPI";
+
+const authentication = {
+  signUp: async (inputs) => {
+    return await callAPI.post("user/signup", inputs).then((data) => data);
+  },
+};
+
+export default authentication;
