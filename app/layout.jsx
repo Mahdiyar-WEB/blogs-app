a56fcdafbf4b73cb6841e8aea6efa739c5199964
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-07 19:47:45 +0330

    add UserProvider in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,23 +2,25 @@ import "styles/globals.css";
 import vazirFont from "constants/localFont";
 import Header from "./Header";
 import { Toaster } from "react-hot-toast";
+import UserProvider from "context/UserContext";
 
 export const metadata = {
   title: {
-    template: '%s | بلاگیتو',
-    default: 'بلاگیتو', // a default is required when creating a template
+    template: "%s | بلاگیتو",
+    default: "بلاگیتو", // a default is required when creating a template
   },
-  description:'وب اپلیکیشن بلاگیتو'
-}
-
+  description: "وب اپلیکیشن بلاگیتو",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl" data-scroll-behavior="smooth">
       <body className={`${vazirFont.variable} font-sans min-h-screen relative`}>
-        <Toaster/>
-        <Header />
-        {children}
+        <Toaster />
+        <UserProvider>
+          <Header />
+          {children}
+        </UserProvider>
       </body>
     </html>
   );
