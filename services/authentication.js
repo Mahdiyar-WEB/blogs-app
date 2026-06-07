a899a630c87cee26190e855bff0b3593c4c89b3d
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-07 18:46:52 +0330

    develop signIn service

--- services/authentication.js
+++ services/authentication.js
@@ -4,6 +4,9 @@ const authentication = {
   signUp: async (inputs) => {
     return await callAPI.post("user/signup", inputs).then((data) => data);
   },
+  signIn: async (inputs) => {
+    return await callAPI.post("user/signin", inputs).then((data) => data);
+  },
 };
 
 export default authentication;
