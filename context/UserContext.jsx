4a846ea5a74dd91a8e2f2def21b23bca29763a6b
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-07 19:49:51 +0330

    develop userContext

---
+++ context/UserContext.jsx
@@ -0,0 +1,104 @@
+"use client";
+import React, { createContext, useContext, useReducer } from "react";
+import authentication from "services/authentication";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  loading: true,
+  error: null,
+};
+
+const UserContext = createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "loading": {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case "rejected": {
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    }
+    case "signUp": {
+      return {
+        user: action.payload,
+        isAuthenticated: true,
+        error: null,
+        loading: false,
+      };
+    }
+    case "signIn": {
+      return {
+        user: action.payload,
+        isAuthenticated: true,
+        error: null,
+        loading: false,
+      };
+    }
+    default:
+      break;
+  }
+};
+
+const UserProvider = ({ children }) => {
+  const router = useRouter();
+  const [{ user, isAuthenticated, loading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const signIn = async (inputs) => {
+    dispatch({ type: "loading" });
+    try {
+      const { data } = await authentication.signIn(inputs);
+      toast.success(data.message);
+      dispatch({ type: "signIn", payload: data.user });
+      router.push("/profile");
+    } catch (error) {
+      const { message } = error;
+      dispatch({ type: "rejected", payload: message });
+      toast.error(message);
+    }
+  };
+
+  const signUp = async (inputs) => {
+    dispatch({ type: "loading" });
+    try {
+      const { data } = await authentication.signUp(inputs);
+      toast.success(data.message);
+      dispatch({ type: "signUp", payload: data.user });
+      router.push("/profile");
+    } catch (error) {
+      const { message } = error;
+      dispatch({ type: "rejected", payload: message });
+      toast.error(message);
+    }
+  };
+
+  const logOut = () => {};
+
+  return (
+    <UserContext.Provider
+      value={{ user, isAuthenticated, loading, error, signIn, signUp, logOut }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) throw new Error("not found user context");
+  return context;
+};
+
+export default UserProvider;
