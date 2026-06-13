2be392ac5bceed1d168c53304b657169e31e541d
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-13 07:50:44 +0330

    use cookies inside getallPosts

--- app/blogs/(postList)/page.jsx
+++ app/blogs/(postList)/page.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Post from "../_components/Post";
-import postServices from "services/postServices";
+import postServices from "api/postServices";
+import generateSSRCookies from "utils/generateSSRCookies";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "بلاگ ها",
 };
 
 const Blogs = async () => {
-  const posts = await postServices.getAllPosts();
+  const cookieStore = await cookies();
+  const posts = await postServices.getAllPosts(generateSSRCookies(cookieStore));
   return (
     <div className="grid grid-cols-12 gap-5">
       {posts.map((post, id) => {
