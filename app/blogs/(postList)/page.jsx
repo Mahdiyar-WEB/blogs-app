0415b7ed44f616a1061e7a476e673ec8e4d8cddb
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-14 10:55:33 +0330

    add search options to blogs page

--- app/blogs/(postList)/page.jsx
+++ app/blogs/(postList)/page.jsx
@@ -3,19 +3,60 @@ import Post from "../_components/Post";
 import postServices from "api/postServices";
 import generateSSRCookies from "utils/generateSSRCookies";
 import { cookies } from "next/headers";
+import queryString from "query-string";
+import Image from "next/image";
+import toPersianDigits from "utils/toPersianDigits";
 
 export const metadata = {
   title: "بلاگ ها",
 };
 
-const Blogs = async () => {
+const Blogs = async ({ searchParams }) => {
   const cookieStore = await cookies();
-  const posts = await postServices.getAllPosts(generateSSRCookies(cookieStore));
+  const options = await searchParams;
+  const posts = await postServices.getAllPosts(
+    generateSSRCookies(cookieStore),
+    queryString.stringify(options),
+  );
   return (
     <div className="grid grid-cols-12 gap-5">
-      {posts.map((post, id) => {
-        return <Post key={id} {...post} />;
-      })}
+      {options?.search && (
+        <>
+          {posts.length === 0 ? (
+            <p className="col-span-12 font-medium flex gap-1">
+              <span>پستی برای جستجوی</span>
+              <span className="text-primary-700 font-semibold">
+                &quot;{options?.search}&quot;
+              </span>
+              <span>پیدا نشد!</span>
+            </p>
+          ) : (
+            <p className="col-span-12 font-medium flex gap-1">
+              <span>{toPersianDigits(posts?.length)}</span>
+              <span>پست بر اساس جستجوی</span>
+              <span className="text-primary-700 font-semibold">
+                &quot;{options?.search}&quot;
+              </span>
+              <span>پیدا شد</span>
+            </p>
+          )}
+        </>
+      )}
+      {!!posts.length === false ? (
+        <div className="col-span-12 flex flex-col items-center justify-center">
+          <Image
+            width={500}
+            height={500}
+            src="/no-blogs.png"
+            sizes="100%"
+            quality={100}
+            alt="no blogs in this category"
+          />
+          <h1 className="font-semibold text-2xl">متاسفانه پستی پیدا نشد!</h1>
+        </div>
+      ) : (
+        posts.map((post, id) => <Post key={id} {...post} />)
+      )}
     </div>
   );
 };
