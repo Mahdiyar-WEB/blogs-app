ab6277e90fb5a1908acd5521473f333536890968
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-16 14:47:22 +0330

    develop CommentForm for addNewComment

--- app/blogs/_components/CommentForm.jsx
+++ app/blogs/_components/CommentForm.jsx
@@ -1,19 +1,26 @@
 "use client";
 import Button from "components/Button";
 import TextArea from "components/TextArea";
+import { createComment } from "lib/actions";
 import React, { useState } from "react";
 
-const CommentForm = ({ parent }) => {
+const CommentForm = ({ parentId, postId }) => {
   const [value, setValue] = useState("");
 
-  const onChangeHandler = (e) => {
-    setValue(e.target.value);
-  };
-
   return (
-    <form className="space-y-5">
-      <TextArea value={value} label='متن نظر' onChange={(e) => onChangeHandler(e)} />
-      <Button className="w-full rounded-sm">{parent ? "ثبت پاسخ" : "ثبت نظر جدید"}</Button>
+    <form
+      className="space-y-5 mt-2"
+      action={createComment.bind(null, postId, parentId)}
+    >
+      <TextArea
+        name="comment"
+        value={value}
+        label="متن نظر"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button type="submit" className="w-full rounded-sm">
+        {parent ? "ثبت پاسخ" : "ثبت نظر جدید"}
+      </Button>
     </form>
   );
 };
