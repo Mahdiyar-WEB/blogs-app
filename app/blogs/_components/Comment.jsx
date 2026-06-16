122ec8204e316ae1ad5b7c2945f2b9e0ec162d82
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-16 11:32:50 +0330

    develop Comment component

---
+++ app/blogs/_components/Comment.jsx
@@ -0,0 +1,45 @@
+import { ArrowUturnRightIcon } from "@heroicons/react/24/outline";
+import Button from "components/Button";
+import Image from "next/image";
+
+function Comment({ comment, onAddComment }) {
+  return (
+    <>
+      <div className="flex items-center justify-between mb-5 border-b border-b-secondary-200/60 pb-2">
+        <div className="flex items-center gap-2">
+          <Image
+            alt={comment.user?.name || "-"}
+            className="rounded-full ring-1 ring-secondary-300"
+            width={34}
+            height={34}
+            src={comment.user.avatarUrl}
+          />
+          <div className="text-sm w-full text-secondary-600">
+            <span className="font-bold block mb-1">{comment.user.name}</span>
+            <span className="block text-secondary-500 text-xs">
+              {comment.createdAt}
+            </span>
+          </div>
+        </div>
+        <div>
+          {comment.openToComment && (
+            <Button
+              onClick={onAddComment}
+              variant="secondary"
+              className="text-sm flex gap-x-1 p-1 rounded-lg text-secondary-500 bg-secondary-200"
+            >
+              <span className="ml-1">
+                <ArrowUturnRightIcon className="w-4" />
+              </span>
+              <span>پاسخ</span>
+            </Button>
+          )}
+        </div>
+      </div>
+      <p className="text-secondary-700 leading-loose lg:leading-8 text-xs lg:text-base">
+        {comment.content.text}
+      </p>
+    </>
+  );
+}
+export default Comment;
