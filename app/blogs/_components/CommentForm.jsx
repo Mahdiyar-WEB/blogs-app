d820bea56a1045d4f7ff943d6e4703a5141421aa
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-16 15:39:09 +0330

    refactor commentForm

--- app/blogs/_components/CommentForm.jsx
+++ app/blogs/_components/CommentForm.jsx
@@ -1,26 +1,45 @@
 "use client";
+import commentServices from "api/commentServices";
 import Button from "components/Button";
+import SubmitButton from "components/SubmitButton";
 import TextArea from "components/TextArea";
-import { createComment } from "lib/actions";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const CommentForm = ({ parentId, postId }) => {
   const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await commentServices.addNewComment({
+        postId,
+        text: value,
+        parentId,
+      });
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <form
-      className="space-y-5 mt-2"
-      action={createComment.bind(null, postId, parentId)}
-    >
+    <form className="space-y-5 mt-2" onSubmit={onSubmitHandler}>
       <TextArea
         name="comment"
         value={value}
         label="متن نظر"
         onChange={(e) => setValue(e.target.value)}
       />
-      <Button type="submit" className="w-full rounded-sm">
+      <SubmitButton
+        loading={loading}
+        className="w-full rounded-md"
+      >
         {parent ? "ثبت پاسخ" : "ثبت نظر جدید"}
-      </Button>
+      </SubmitButton>
     </form>
   );
 };
