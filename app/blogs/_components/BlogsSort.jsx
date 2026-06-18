f8488999c6bd72f31f989c44af3ce26113edcb16
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-18 05:54:17 +0330

    develop BlogsSort for blogs page

---
+++ app/blogs/_components/BlogsSort.jsx
@@ -0,0 +1,64 @@
+// import { useState } from "react";
+"use client";
+import { useCallback, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Select from "components/Select";
+
+const sortOptions = [
+  {
+    label: "تاریخ ایجاد (جدید ترین)",
+    value: "latest",
+  },
+  {
+    label: "تاریخ ایجاد (قدیمی ترین)",
+    value: "earliest",
+  },
+  {
+    label: "محبوبیت",
+    value: "popular",
+  },
+  {
+    label: "زمان مطالعه (نزولی)",
+    value: "time_desc",
+  },
+  {
+    label: "زمان مطالعه (صعودی)",
+    value: "time_asc",
+  },
+];
+
+const BlogsSort = () => {
+  const searchParams = useSearchParams();
+  const [sort, setSort] = useState(searchParams.get("sort") || "latest");
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  return (
+    <div className="h-11 w-full md:w-1/2 xl:w-1/3 flex justify-between bg-white/50 border border-secondary-200 rounded-lg shadow-md">
+      <Select
+        onChange={(e) => {
+          setSort(e.target.value);
+          router.push(
+            pathname + "?" + createQueryString("sort", e.target.value),
+          );
+        }}
+        value={sort}
+        options={sortOptions}
+      />
+    </div>
+  );
+};
+export default BlogsSort;
