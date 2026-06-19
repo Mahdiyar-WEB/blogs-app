6ae6308c46dfa81621e19eadafd742fcd9d9bfdf
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-19 14:49:36 +0330

    add Suspense and LoadingHeader in root Layout

--- app/(website)/layout.jsx
+++ app/(website)/layout.jsx
@@ -1,9 +1,6 @@
-import "styles/globals.css";
-import vazirFont from "constants/localFont";
 import Header from "./Header";
-import { Toaster } from "react-hot-toast";
-import UserProvider from "context/UserContext";
 import { Suspense } from "react";
+import Link from "next/link";
 
 export const metadata = {
   title: {
@@ -15,11 +12,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <div className={`${vazirFont.variable} font-sans min-h-screen relative`}>
-      <Suspense>
+    <div className={`font-sans min-h-screen relative`}>
+      <Suspense fallback={<LoadingHeader />}>
         <Header />
       </Suspense>
       {children}
     </div>
   );
 }
+
+const LoadingHeader = () => {
+  return (
+    <header className="py-3 shadow-md border border-secondary-200 rounded-md w-11/12 mx-auto 2xl:max-w-screen-2xl sticky top-3 left-0 bg-secondary-50 z-10 mb-10">
+      <nav className="mx-3 lg:mx-10 flex justify-between">
+        <ul className="flex gap-10">
+          <li>
+            <Link href="/">خانه</Link>
+          </li>
+          <li>
+            <Link href="/blogs">بلاگ ها</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
