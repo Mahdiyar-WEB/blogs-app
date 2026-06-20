ca18dfd74f6ba629bd2d3f650cde27d0f6e6057a
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-20 11:05:44 +0330

    add getAllComments API to commentServices

--- api/commentServices.js
+++ api/commentServices.js
@@ -4,6 +4,9 @@ const commentServices = {
   addNewComment: async (values) => {
     return await callAPI.post("comment/add", values);
   },
+  getAllComments: async (cookies) => {
+    return await callAPI.get("comment/list", cookies);
+  },
 };
 
 export default commentServices;
