39c1a12903fd5ef9409428ff9f0b6a116b191f63
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-20 13:22:37 +0330

    develop PostsInformation.jsx for admin panel route group

---
+++ app/(admin panel)/_components/PostsInformation.jsx
@@ -0,0 +1,123 @@
+"use client";
+import postServices from "api/postServices";
+import ButtonIcon from "components/ButtonIcon";
+import Table from "components/Table";
+import React, { useEffect, useState } from "react";
+import generateSSRCookies from "utils/generateSSRCookies";
+import toLocalDateShort from "utils/toLocalDateShort";
+import toPersianDigits from "utils/toPersianDigits";
+import truncateText from "utils/truncateText";
+import DeletePostModal from "./DeletePostModal";
+
+const postTypeValues = {
+  free: {
+    text: "رایگان",
+    style: "badge--success",
+  },
+  premium: {
+    text: "پولی",
+    style: "badge--secondary",
+  },
+};
+
+const PostsInformation = () => {
+  const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPost] = useState({});
+  const [postAction, setPostAction] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await postServices.getAllPosts(
+        generateSSRCookies(cookieStore),
+      );
+      setPosts(posts);
+    };
+    fetchPosts();
+  }, []);
+
+  const postActionHandler = (post, action) => {
+    setSelectedPost(post);
+    setPostAction(action);
+  };
+
+  const onClosePostAction = () => {
+    setPostAction("");
+    setPostAction({});
+  };
+
+  return (
+    <section>
+      <h3 className="font-medium text-xl mb-5">اطلاعات پست ها</h3>
+      <Table>
+        <Table.Header>
+          <th>#</th>
+          <th>عنوان</th>
+          <th>دسته بندی</th>
+          <th>نویسنده</th>
+          <th>تاریخ ایجاد</th>
+          <th>نوع</th>
+          <th>عملیات</th>
+        </Table.Header>
+        <Table.Body>
+          {posts.map((post, index) => {
+            return (
+              <Table.Row key={post._id}>
+                <td>{toPersianDigits(index + 1)}</td>
+                <td>{truncateText(post.title, 25)}</td>
+                <td>{post.category?.title}</td>
+                <td>{post.author?.name}</td>
+                <td>{toLocalDateShort(post.createdAt)}</td>
+                <td>
+                  <span className={`badge ${postTypeValues[post.type].style}`}>
+                    {postTypeValues[post.type].text}
+                  </span>
+                </td>
+                <td>
+                  <div className="flex gap-3">
+                    <ButtonIcon
+                      variant="primary"
+                      onClick={() => postActionHandler(post, "edit")}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="size-5"
+                      >
+                        <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                      </svg>
+                    </ButtonIcon>
+                    <ButtonIcon
+                      variant="red"
+                      onClick={() => postActionHandler(post, "delete")}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="size-5"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </ButtonIcon>
+                  </div>
+                </td>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+      <DeletePostModal
+        open={selectedPost && postAction === "delete"}
+        post={selectedPost}
+        onClose={onClosePostAction}
+      />
+    </section>
+  );
+};
+
+export default PostsInformation;
