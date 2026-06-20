0b21d2bf28edc82e68aeb3a13e8ac75044409e91
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-20 13:22:01 +0330

    develop DeletePostModal

---
+++ app/(admin panel)/_components/DeletePostModal.jsx
@@ -0,0 +1,25 @@
+import Button from "components/Button";
+import Modal from "components/Modal";
+import React from "react";
+
+const DeletePostModal = ({ open, post, onClose }) => {
+  return (
+    <Modal open={open} title="حذف پست" onClose={onClose}>
+      <p className="flex gap-2 mb-5">
+        <span>آیا از حذف پست</span>
+        <span className="font-semibold">&quot;{post.title}&quot;</span>
+        <span>اطمینان دارید؟</span>
+      </p>
+      <div className="flex gap-3 justify-end">
+        <Button className="px-4 py-2" variant="secondary" onClick={onClose}>
+          خیر
+        </Button>
+        <Button className="px-5 py-2" variant="danger">
+          بله
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default DeletePostModal;
