edc70cf6a194ca130fdf78e7b3e6fd15372f9593
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-20 14:17:04 +0330

    remove title from PostsInformation

--- app/(admin panel)/profile/blogs/page.jsx
+++ app/(admin panel)/profile/blogs/page.jsx
@@ -4,7 +4,7 @@ import PostsInformation from "../../_components/PostsInformation";
 const page = () => {
   return (
     <main className="p-5">
-      <PostsInformation title='پست ها' />
+      <PostsInformation />
     </main>
   );
 };
