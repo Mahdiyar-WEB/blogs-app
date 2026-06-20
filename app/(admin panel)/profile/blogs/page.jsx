f0d8ce286d67681101d12b3ad8b85c295af9cc1c
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-20 13:32:50 +0330

    develop /profile/blogs route

---
+++ app/(admin panel)/profile/blogs/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostsInformation from "../../_components/PostsInformation";
+
+const page = () => {
+  return (
+    <main className="p-5">
+      <PostsInformation title='پست ها' />
+    </main>
+  );
+};
+
+export default page;
