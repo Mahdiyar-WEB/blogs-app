4bbb811b837f312c4eccfa43de7c70f1951d1c71
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-21 15:50:37 +0330

    remove Suspense from PostComments

--- app/(website)/blogs/_components/PostComments.jsx
+++ app/(website)/blogs/_components/PostComments.jsx
@@ -83,37 +83,35 @@ function PostComments({ post: { comments = [], _id: postId = "" } }) {
       </div>
       <div className="space-y-8 post-comments bg-secondary-0 rounded-xl py-6 px-3 lg:px-6 ">
         {comments.length > 0 ? (
-          <Suspense>
-            {comments.map((comment) => {
-              return (
-                <div key={comment._id}>
-                  <div className="border border-secondary-200 rounded-xl p-2 sm:p-4 mb-3">
-                    <Comment
-                      comment={comment}
-                      onAddComment={() => addNewCommentHandler(comment)}
-                    />
-                  </div>
-                  <div className="post-comments__answer mr-2 sm:mr-8 space-y-3">
-                    {comment.answers.map((item, index) => {
-                      return (
-                        <div key={item._id} className="relative">
-                          <div
-                            className={`answer-item border border-secondary-100 bg-secondary-50/80 rounded-xl p-2 sm:p-4 ${index + 1 === comment.answers.length && "last-item"}`}
-                          >
-                            <Comment
-                              onAddComment={() => addNewCommentHandler(comment)}
-                              comment={item}
-                              key={item._id}
-                            />
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
+          comments.map((comment) => {
+            return (
+              <div key={comment._id}>
+                <div className="border border-secondary-200 rounded-xl p-2 sm:p-4 mb-3">
+                  <Comment
+                    comment={comment}
+                    onAddComment={() => addNewCommentHandler(comment)}
+                  />
                 </div>
-              );
-            })}
-          </Suspense>
+                <div className="post-comments__answer mr-2 sm:mr-8 space-y-3">
+                  {comment.answers.map((item, index) => {
+                    return (
+                      <div key={item._id} className="relative">
+                        <div
+                          className={`answer-item border border-secondary-100 bg-secondary-50/80 rounded-xl p-2 sm:p-4 ${index + 1 === comment.answers.length && "last-item"}`}
+                        >
+                          <Comment
+                            onAddComment={() => addNewCommentHandler(comment)}
+                            comment={item}
+                            key={item._id}
+                          />
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })
         ) : (
           <p className="text-secondary-500">برای این پست نظری ثبت نشده است</p>
         )}
