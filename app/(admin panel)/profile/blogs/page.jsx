cd2d1d8d999c78ef13fe5fb42f8ded51f103ff0f
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-24 10:01:18 +0330

    add create post link

--- app/(admin panel)/profile/blogs/page.jsx
+++ app/(admin panel)/profile/blogs/page.jsx
@@ -6,6 +6,9 @@ import postServices from "api/postServices";
 import { cookies } from "next/headers";
 import generateSSRCookies from "utils/generateSSRCookies";
 import Pagination from "components/Pagination";
+import Button from "components/Button";
+import ButtonIcon from "components/ButtonIcon";
+import Link from "next/link";
 
 const page = async ({ searchParams }) => {
   const query = queryString.stringify(await searchParams);
@@ -16,9 +19,32 @@ const page = async ({ searchParams }) => {
   );
   return (
     <main className="p-5">
-      <Suspense>
-        <SearchBox />
-      </Suspense>
+      <div className="flex justify-between items-center">
+        <Suspense>
+          <SearchBox />
+        </Suspense>
+        <Link
+          href="/profile/blogs/create"
+          className="border flex gap-1 items-center bg-primary-800 text-secondary-0 px-4 rounded-md py-2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+
+          <span>ایجاد پست</span>
+        </Link>
+      </div>
       <PostsInformation fetchQueries={query} />
       <Pagination totalPages={totalPages} />
     </main>
