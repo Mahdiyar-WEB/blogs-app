c7fc7f8bd7892f6aac6c5968705a50aecd24e414
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-26 18:24:21 +0330

    update useGetPosts hook

--- hooks/useGetPosts.js
+++ hooks/useGetPosts.js
@@ -1,12 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import postServices from "api/postServices";
 
-const useGetPosts = () => {
-  const { data: posts, isLoading } = useQuery({
-    queryFn: postServices.getAllPosts,
-    queryKey: ["get-posts"],
+const useGetPosts = (params) => {
+  const { data = {}, isLoading } = useQuery({
+    queryFn: () => {
+      return postServices.getAllPosts("", params);
+    },
+    queryKey: ["get-posts", params],
   });
-  return { posts, isLoading };
+  return { posts: data.posts, isLoading };
 };
 
 export default useGetPosts;
