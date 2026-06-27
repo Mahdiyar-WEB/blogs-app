148e405949e3a23d2689a1e1407d5221fdc0bdf4
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-27 13:53:33 +0330

    fix the issue of re-fetching after delete post

--- app/(admin panel)/_components/DeletePostModal.jsx
+++ app/(admin panel)/_components/DeletePostModal.jsx
@@ -1,8 +1,24 @@
+import { useQueryClient } from "@tanstack/react-query";
+import postServices from "api/postServices";
 import Button from "components/Button";
 import Modal from "components/Modal";
 import React from "react";
+import toast from "react-hot-toast";
 
 const DeletePostModal = ({ open, post, onClose }) => {
+  const queryClient = useQueryClient();
+  
+  const deletePostHandler = async () => {
+    try {
+      const { data } = await postServices.deletePost(post._id);
+      toast.success(data.message);
+      queryClient.invalidateQueries({ queryKey: ["get-posts"] });
+      onClose();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <Modal open={open} title="حذف پست" onClose={onClose}>
       <p className="flex gap-2 mb-5">
@@ -14,7 +30,11 @@ const DeletePostModal = ({ open, post, onClose }) => {
         <Button className="px-4 py-2" variant="secondary" onClick={onClose}>
           خیر
         </Button>
-        <Button className="px-5 py-2" variant="danger">
+        <Button
+          className="px-5 py-2"
+          variant="danger"
+          onClick={() => deletePostHandler()}
+        >
           بله
         </Button>
       </div>
