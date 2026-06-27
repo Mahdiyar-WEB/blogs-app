b62fb0ead88eb93d99e433d35ac219baefc72edd
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-27 21:02:07 +0330

    develop profile/comments page

---
+++ app/(admin panel)/profile/comments/page.jsx
@@ -0,0 +1,27 @@
+import commentServices from "api/commentServices";
+import BreadCrumbs from "components/BreadCrumbs";
+import Pagination from "components/Pagination";
+import { cookies } from "next/headers";
+import React, { Suspense } from "react";
+import CommentsInformation from "../../_components/CommentsInformation";
+import SearchBox from "components/SearchBox";
+import queryString from "query-string";
+
+const CommentsPage = async ({ searchParams }) => {
+  const query = queryString.stringify(await searchParams);
+  const { data } = await commentServices.getAllComments("", query);
+
+  return (
+    <main className="p-5">
+      <BreadCrumbs />
+      <Suspense>
+        <SearchBox />
+      </Suspense>
+      <CommentsInformation comments={data.comments} />
+
+      <Pagination totalPages={data.totalPages} />
+    </main>
+  );
+};
+
+export default CommentsPage;
