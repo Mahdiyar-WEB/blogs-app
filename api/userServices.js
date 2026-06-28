ebbfb4669e619c991fa302b3bf273cdc790a9edc
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-28 20:53:20 +0330

    develop getUserById and updateUser

--- api/userServices.js
+++ api/userServices.js
@@ -7,6 +7,12 @@ const userServices = {
   deleteUser: async (id) => {
     return await callAPI.delete(`user/remove/${id}`);
   },
+  getUserById: async (cookies, id = "") => {
+    return await callAPI.get(`user/profile/${id}`, cookies);
+  },
+  updateUser: async ({ id, data }) => {
+    return await callAPI.patch(`user/update/${id}`, data);
+  },
 };
 
 export default userServices;
