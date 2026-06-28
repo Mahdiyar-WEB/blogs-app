d8f46d4914160c5576e69a4cfe8551e784247c4d
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-28 16:48:35 +0330

    develop /profile/users page

---
+++ app/(admin panel)/profile/users/page.jsx
@@ -0,0 +1,31 @@
+import BreadCrumbs from "components/BreadCrumbs";
+import Pagination from "components/Pagination";
+import { cookies } from "next/headers";
+import React, { Suspense } from "react";
+import SearchBox from "components/SearchBox";
+import queryString from "query-string";
+import userServices from "api/userServices";
+import generateSSRCookies from "utils/generateSSRCookies";
+import UsersInformation from "../../_components/UsersInformation";
+
+const UsersPage = async ({ searchParams }) => {
+  const query = queryString.stringify(await searchParams);
+  const cookieStore = await cookies();
+
+  const { data } = await userServices.getAllUsers(
+    generateSSRCookies(cookieStore),
+    query,
+  );
+  return (
+    <main className="p-5">
+      <BreadCrumbs />
+      <Suspense>
+        <SearchBox />
+      </Suspense>
+      <UsersInformation fetchQueries={query} />
+      <Pagination totalPages={data.totalPages} />
+    </main>
+  );
+};
+
+export default UsersPage;
