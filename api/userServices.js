9fef1fce39373b0fb0dced699a17af6f6ec3afa7
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-28 17:01:31 +0330

    develop delete user api with react query hook

--- api/userServices.js
+++ api/userServices.js
@@ -4,6 +4,9 @@ const userServices = {
   getAllUsers: async (cookies, searchOptions = "") => {
     return await callAPI.get(`user/list?${searchOptions}`, cookies);
   },
+  deleteUser: async (id) => {
+    return await callAPI.delete(`user/${id}`);
+  },
 };
 
 export default userServices;
