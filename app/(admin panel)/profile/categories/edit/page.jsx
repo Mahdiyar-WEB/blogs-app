4bdd25eabe98d2f3024eaa66ad5c82609e1c2117
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-29 23:13:05 +0330

    develop edit category page

---
+++ app/(admin panel)/profile/categories/edit/page.jsx
@@ -0,0 +1,37 @@
+import BreadCrumbs from "components/BreadCrumbs";
+import React from "react";
+import { notFound } from "next/navigation";
+import categoryServices from "api/categoryServices";
+import EditCategoryForm from "./EditCategoryForm";
+
+const fetchCategoryByTitle = async (categoryTitle) => {
+  try {
+    const data = await categoryServices.getCategoryByTitle(categoryTitle);
+    return data;
+  } catch (error) {
+    notFound();
+  }
+};
+
+const EditCategoryPage = async ({ searchParams }) => {
+  const { categoryTitle } = await searchParams;
+  const {
+    data: { category = {} },
+  } = await fetchCategoryByTitle(categoryTitle);
+
+  return (
+    <main className="p-5">
+      <BreadCrumbs slugTitle="ویرایش دسته بندی"/>
+      <EditCategoryForm
+        initialValues={{
+          title: category.title,
+          englishTitle: category.englishTitle,
+          description: category.description,
+        }}
+        categoryId={category._id}
+      />
+    </main>
+  );
+};
+
+export default EditCategoryPage;
