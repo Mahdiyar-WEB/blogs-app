5ac44b05e3d241e87b41667280e5404f3652410b
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-29 23:11:42 +0330

    develop deleteCategory,getCategoryByTitle,updateCategory in categoryServices

--- api/categoryServices.js
+++ api/categoryServices.js
@@ -5,6 +5,16 @@ const categoryServices = {
     const data = await callAPI.get(`category/list?${searchOptions}`);
     return data;
   },
+  deleteCategory: async (id) => {
+    return await callAPI.delete(`category/remove/${id}`);
+  },
+  getCategoryByTitle: async (title = "") => {
+    const data = await callAPI.get(`category/${title}`);
+    return data;
+  },
+  updateCategory: async ({ id, data }) => {
+    return await callAPI.patch(`category/update/${id}`, data);
+  },
 };
 
 export default categoryServices;
