490df864d77895b25f812543b8e8e289f469dec4
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-29 23:13:52 +0330

    develop useUpdateCategory hook

---
+++ hooks/categories/useUpdateCategory.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import categoryServices from "api/categoryServices";
+import postServices from "api/postServices";
+import toast from "react-hot-toast";
+
+const useUpdateCategory = () => {
+  const queryClient = useQueryClient();
+  const { mutate: updateCategory, isPending: isUpdating } = useMutation({
+    mutationFn: categoryServices.updateCategory,
+    onSuccess: ({ data }) => {
+      toast.success(data?.message);
+      queryClient.invalidateQueries({
+        queryKey: ["get-categories"],
+      });
+    },
+    onError: (error) => {
+      toast.error(error?.message);
+    },
+  });
+
+  return { updateCategory, isUpdating };
+};
+
+export default useUpdateCategory;
