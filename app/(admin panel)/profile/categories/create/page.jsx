41ecab9b98e2d0d097932e65b0b0f2d38434c5f6
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-06-30 21:19:35 +0330

    develop create category and CreateCategoryForm component

---
+++ app/(admin panel)/profile/categories/create/page.jsx
@@ -0,0 +1,14 @@
+import BreadCrumbs from "components/BreadCrumbs";
+import React from "react";
+import CreateCategoryForm from "./CreateCategoryForm";
+
+const CreateCategoryPage = () => {
+  return (
+    <main className="p-5">
+      <BreadCrumbs slugTitle="ایجاد دسته بندی" />
+      <CreateCategoryForm />
+    </main>
+  );
+};
+
+export default CreateCategoryPage;
