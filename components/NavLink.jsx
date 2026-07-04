b33b11ff71c161aa7b08dd75b92d0fb823077ba3
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-04 18:41:01 +0330

    refactor NavLink component for mobile size

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -4,25 +4,32 @@ import React from "react";
 import { usePathname } from "next/navigation";
 
 const NavLink = ({
-  path='/',
+  path = "/",
   text,
   className,
   listOption = false,
+  mobile = false,
   prefetch = "auto",
 }) => {
   const pathName = usePathname();
+  const active = pathName === path;
   return (
     <Link
       prefetch={prefetch}
-      className={`font-medium  transition-all duration-200 ease-out ${
-        listOption && "px-2 py-1 hover:ps-3"
-      } ${
-        pathName === path && listOption
-          ? "text-white bg-primary-900 rounded-md"
-          : pathName === path && !listOption
+      className={`font-medium transition-all duration-200 ease-out ${listOption && "px-2 py-1 hover:ps-3"} ${mobile && `shrink-0 rounded-full border px-4 py-2 text-sm whitespace-nowrap `}
+    ${
+      active && listOption
+        ? "text-white bg-primary-900 rounded-md"
+        : active && mobile
+          ? "bg-primary-900 text-white border-primary-900"
+          : active
             ? "text-primary-900"
-            : "text-secondary-400 hover:text-secondary-900"
-      } ${className}`}
+            : mobile
+              ? "bg-white border-secondary-200 text-secondary-600 hover:border-secondary-300"
+              : "text-secondary-400 hover:text-secondary-900"
+    }
+    ${className || ""}
+  `}
       href={path}
     >
       {text}
