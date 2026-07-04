d184095bcfc00733ba87879e433d05784ccb6de3
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-04 18:39:09 +0330

    refactor categoryList for mobile size

--- app/(website)/blogs/_components/CategoryList.jsx
+++ app/(website)/blogs/_components/CategoryList.jsx
@@ -2,23 +2,41 @@ import NavLink from "components/NavLink";
 import React from "react";
 import categoryServices from "api/categoryServices";
 
-const CategoryList = async () => {
+const CategoryList = async ({ mobile = false }) => {
   const { data } = await categoryServices.getAllCategories();
+
+  if (mobile) {
+    return (
+      <>
+        <NavLink mobile path="/blogs" text="همه" />
+
+        {data?.categories.map(({ title, slug, _id }) => (
+          <NavLink
+            key={_id}
+            mobile
+            prefetch={false}
+            path={`/blogs/category/${slug}`}
+            text={title}
+          />
+        ))}
+      </>
+    );
+  }
+
   return (
     <ul className="text-md md:text-lg space-y-3 ms-3">
       <NavLink listOption path="/blogs" text="همه" />
-      {data?.categories.map(({ title, slug, _id }) => {
-        return (
-          <li key={_id}>
-            <NavLink
-              prefetch={false}
-              listOption
-              path={`/blogs/category/${slug}`}
-              text={title}
-            />
-          </li>
-        );
-      })}
+
+      {data?.categories.map(({ title, slug, _id }) => (
+        <li key={_id}>
+          <NavLink
+            prefetch={false}
+            listOption
+            path={`/blogs/category/${slug}`}
+            text={title}
+          />
+        </li>
+      ))}
     </ul>
   );
 };
