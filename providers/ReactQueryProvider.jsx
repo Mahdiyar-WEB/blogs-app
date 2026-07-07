dcac3202c4fe0e0ed1244cbddfae306f45302f11
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-08 01:10:02 +0330

    remove react query dev tools

--- providers/ReactQueryProvider.jsx
+++ providers/ReactQueryProvider.jsx
@@ -1,12 +1,10 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 const ReactQueryProvider = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       {children}
     </QueryClientProvider>
   );
