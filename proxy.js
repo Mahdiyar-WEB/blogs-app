77315b557b5928543e257368d1a9a3e3b2175325
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-12 02:19:59 +0330

    update proxy for handle profile routes

--- proxy.js
+++ proxy.js
@@ -3,12 +3,18 @@ import middlewareAuth from "utils/middlewareAuth";
 
 export async function proxy(request) {
   const pathName = request.nextUrl.pathname;
+
   const user = await middlewareAuth(request);
+
   if (pathName === "/login" && user) {
     return NextResponse.redirect(new URL("/", request.url));
-  } else if (pathName === "/profile" && !user) {
+  }
+
+  if (pathName.startsWith("/profile") && !user) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
