ea62105b341d2d102f16aa25603685b11fbc2725
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-12 02:20:50 +0330

    add condition to deleteUserHandler

--- app/(admin panel)/_components/DeleteUserModal.jsx
+++ app/(admin panel)/_components/DeleteUserModal.jsx
@@ -1,21 +1,31 @@
 import Button from "components/Button";
 import Modal from "components/Modal";
+import { useUser } from "context/UserContext";
 import useDeleteUser from "hooks/users/useDeleteUser";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const DeleteUserModal = ({ user, onClose }) => {
+const DeleteUserModal = ({ user: selectedUser, onClose }) => {
   const { deleteUser } = useDeleteUser();
+  const router = useRouter();
+  const { user, logout } = useUser();
 
   const deleteUserHandler = async () => {
-    deleteUser(user?._id);
+    if (user?._id === selectedUser?._id) {
+      deleteUser(selectedUser?._id);
+      logout();
+      router.push("/");
+    } else {
+      deleteUser(selectedUser?._id);
+    }
     onClose();
   };
 
   return (
-    <Modal open={!!user} title="حذف پست" onClose={onClose}>
+    <Modal open={!!selectedUser} title="حذف پست" onClose={onClose}>
       <p className="flex gap-2 mb-5">
         <span>آیا از حذف کاربر</span>
-        <span className="font-semibold">&quot;{user?.name}&quot;</span>
+        <span className="font-semibold">&quot;{selectedUser?.name}&quot;</span>
         <span>اطمینان دارید؟</span>
       </p>
       <div className="flex gap-3 justify-end">
