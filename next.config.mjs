48321e3cb8d71e4ab5699a21da3e3d989e4a5d5c
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-12 04:31:14 +0330

    remove images url parameters from next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  cacheComponents:true,
+  cacheComponents: true,
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [new URL("http://localhost:5000/uploads/**")],
-    dangerouslyAllowLocalIP: true,
-  },
   logging: {
     fetches: {
       fullUrl: true,
