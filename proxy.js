02022828dc92bb07ccba1f12cb25e12b23165b22
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-13 08:01:20 +0330

    new config for proxy.js

--- proxy.js
+++ proxy.js
@@ -3,11 +3,10 @@ import middlewareAuth from "utils/middlewareAuth";
 
 export async function proxy(request) {
   const pathName = request.nextUrl.pathname;
-
   const user = await middlewareAuth(request);
 
   if (pathName === "/login" && user) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/profile", request.url));
   }
 
   if (pathName.startsWith("/profile") && !user) {
