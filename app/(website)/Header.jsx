9ae1338757d29b06e2bc7c95a57f75202f53e9c7
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-17 09:56:47 +0330

    increase gap and mx for header NavLinks on mobile size

--- app/(website)/Header.jsx
+++ app/(website)/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
   return (
     <header className="py-3 shadow-md border border-secondary-200 rounded-md w-11/12 mx-auto 2xl:max-w-screen-2xl sticky top-3 left-0 bg-white z-10 mb-10">
-      <nav className="mx-3 lg:mx-10 flex justify-between">
+      <nav className="mx-5 lg:mx-10 flex justify-between">
         <ul className="flex gap-4 md:gap-8">
           <li>
             <NavLink path="/" text="خانه" />
