3cea2883e8fa7d60a30a22a732099057fbffe0c1
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-17 15:30:40 +0330

    refactor get post list api

--- app/api/post/list/route.js
+++ app/api/post/list/route.js
@@ -4,7 +4,6 @@ import { PostModel } from "lib/models/Post";
 import { CategoryModel } from "lib/models/Category";
 import { getUserFromRequest } from "lib/auth";
 import { withErrorHandler, ok } from "lib/apiHandler";
-import { copyObject } from "lib/utils";
 import { transformPost } from "lib/transformPost";
 
 export const GET = withErrorHandler(async (req) => {
@@ -20,7 +19,7 @@ export const GET = withErrorHandler(async (req) => {
 
   const skip = (page - 1) * limit;
 
-  let dbQuery = {};
+  const dbQuery = {};
 
   if (search) {
     const searchTerm = new RegExp(search, "ig");
@@ -37,9 +36,7 @@ export const GET = withErrorHandler(async (req) => {
     const categoryIds = [];
 
     for (const item of categories) {
-      const category = await CategoryModel.findOne({ slug: item }).select(
-        "_id",
-      );
+      const category = await CategoryModel.findOne({ slug: item }).select("_id");
       if (category) categoryIds.push(category._id);
     }
 
@@ -74,7 +71,12 @@ export const GET = withErrorHandler(async (req) => {
         { path: "category", select: { title: 1, slug: 1 } },
         {
           path: "author",
-          select: { name: 1, biography: 1, avatar: 1, avatarBlurDataURL: 1 },
+          select: {
+            name: 1,
+            biography: 1,
+            avatar: 1,
+            avatarBlurDataURL: 1,
+          },
         },
         {
           path: "related",
@@ -84,7 +86,9 @@ export const GET = withErrorHandler(async (req) => {
             slug: 1,
             briefText: 1,
             coverImage: 1,
+            coverImageBlurDataURL: 1,
             author: 1,
+            category: 1,
           },
           populate: [
             {
@@ -114,7 +118,7 @@ export const GET = withErrorHandler(async (req) => {
 
   const totalPages = Math.ceil(totalPosts / limit);
 
-  const transformedPosts = copyObject(posts);
+  const transformedPosts = posts.map((post) => post.toJSON());
 
   for (const post of transformedPosts) {
     await transformPost(post, user);
