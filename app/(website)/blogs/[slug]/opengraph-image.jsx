905e0f16173ff6e32fcfd1dd00cc6edad2d58eec
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-18 14:52:29 +0330

    add alt to opengrapgh image

--- app/(website)/blogs/[slug]/opengraph-image.jsx
+++ app/(website)/blogs/[slug]/opengraph-image.jsx
@@ -14,81 +14,79 @@ export default async function Image({ params }) {
   const post = await postServices.getPostBySlug(slug);
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        position: "relative",
+        background: "#0f172a",
+      }}
+    >
+      <img
+        src={post.coverImageUrl}
+        alt={post.title}
+        width="1200"
+        height="630"
+        style={{
+          position: "absolute",
+          inset: 0,
+          objectFit: "cover",
+        }}
+      />
+
       <div
         style={{
-          width: "100%",
-          height: "100%",
+          position: "absolute",
+          inset: 0,
+          background:
+            "linear-gradient(to top, rgba(0,0,0,.85), rgba(0,0,0,.3))",
+        }}
+      />
+
+      <div
+        style={{
           display: "flex",
-          position: "relative",
-          background: "#0f172a",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          padding: "60px",
+          color: "#fff",
+          zIndex: 1,
+          width: "100%",
         }}
       >
-        <img
-          src={post.coverImageUrl}
-          alt=""
-          width="1200"
-          height="630"
-          style={{
-            position: "absolute",
-            inset: 0,
-            objectFit: "cover",
-          }}
-        />
-
         <div
           style={{
-            position: "absolute",
-            inset: 0,
-            background:
-              "linear-gradient(to top, rgba(0,0,0,.85), rgba(0,0,0,.3))",
-          }}
-        />
-
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-end",
-            padding: "60px",
-            color: "#fff",
-            zIndex: 1,
-            width: "100%",
+            fontSize: 28,
+            opacity: 0.85,
+            marginBottom: 20,
           }}
         >
-          <div
-            style={{
-              fontSize: 28,
-              opacity: 0.85,
-              marginBottom: 20,
-            }}
-          >
-            {post.category.title}
-          </div>
+          {post.category.title}
+        </div>
 
-          <div
-            style={{
-              fontSize: 64,
-              lineHeight: 1.2,
-              fontWeight: 700,
-              maxWidth: "1000px",
-            }}
-          >
-            {post.title}
-          </div>
+        <div
+          style={{
+            fontSize: 64,
+            lineHeight: 1.2,
+            fontWeight: 700,
+            maxWidth: "1000px",
+          }}
+        >
+          {post.title}
+        </div>
 
-          <div
-            style={{
-              fontSize: 24,
-              marginTop: 24,
-              opacity: 0.8,
-            }}
-          >
-            belagito.ir
-          </div>
+        <div
+          style={{
+            fontSize: 24,
+            marginTop: 24,
+            opacity: 0.8,
+          }}
+        >
+          belagito.ir
         </div>
       </div>
-    ),
-    size
+    </div>,
+    size,
   );
 }
