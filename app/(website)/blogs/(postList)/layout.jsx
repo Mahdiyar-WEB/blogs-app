52ee9b56d04e6a20587d7cdad3d091669db39b0f
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-27 22:33:46 +0330

    add fallback for category list suspense

--- app/(website)/blogs/(postList)/layout.jsx
+++ app/(website)/blogs/(postList)/layout.jsx
@@ -36,7 +36,7 @@ const Layout = ({ children }) => {
         </aside>
         <div className="md:hidden relative mb-2 col-span-12">
           <div className="flex gap-2 overflow-x-auto whitespace-nowrap scrollbar-hide pb-2 pe-14">
-            <Suspense>
+            <Suspense fallback={<CategoryListLoading mobile />}>
               <CategoryList mobile />
             </Suspense>
           </div>
