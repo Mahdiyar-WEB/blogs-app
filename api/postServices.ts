3cd391e65eac5d0d44d4acc8229f77f650a13a65
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-12 17:51:39 +0330

    update postServices api

--- api/postServices.ts
+++ api/postServices.ts
@@ -1,40 +1,44 @@
 import callAPI from "api/callAPI";
 
 const postServices = {
-  getPostBySlug: async (postSlug) => {
+  getPostBySlug: async (postSlug: string) => {
     const { data = {} } = await callAPI.get(`post/slug/${postSlug}`);
     const { post } = data || null;
     return post;
   },
-  getPostById: async (id) => {
+  getPostById: async (id: string) => {
     const { data = {} } = await callAPI.get(`post/${id}`);
     const { post } = data || null;
     return post;
   },
-  getAllPosts: async (cookies, searchOption) => {
+  getAllPosts: async (cookies: string, searchOption: string) => {
     const { data } = await callAPI.get(`post/list?${searchOption}`, cookies);
     return data;
   },
-  getPostsByCategory: async (category = "", searchOption, cookies) => {
+  getPostsByCategory: async (
+    category = "",
+    searchOption: string,
+    cookies: string,
+  ) => {
     const { data } = await callAPI.get(
       `post/list?categorySlug=${category}&${searchOption}`,
       cookies,
     );
     return data;
   },
-  likePost: async (id) => {
-    return await callAPI.post(`post/like/${id}`);
+  likePost: async (id: string) => {
+    return await callAPI.post(`post/like/${id}`, {});
   },
-  bookmarkPost: async (id) => {
-    return await callAPI.post(`post/bookmark/${id}`);
+  bookmarkPost: async (id: string) => {
+    return await callAPI.post(`post/bookmark/${id}`, {});
   },
-  deletePost: async (id) => {
+  deletePost: async (id: string) => {
     return await callAPI.delete(`post/remove/${id}`);
   },
-  createPost: async (data) => {
+  createPost: async (data: FormData) => {
     return await callAPI.post("post/create", data);
   },
-  updatePost: async ({ id, data }) => {
+  updatePost: async ({ id, data }: { id: string; data: FormData }) => {
     return await callAPI.patch(`post/update/${id}`, data);
   },
 };
