567c95c24703ba84b2d1f6c9d0d93c7b1e62c61a
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-12 17:51:13 +0330

    update CategoryServices api

--- api/categoryServices.ts
+++ api/categoryServices.ts
@@ -1,3 +1,5 @@
+import { Category } from "lib/models/Category";
+
 const { default: callAPI } = require("api/callAPI");
 
 const categoryServices = {
@@ -5,17 +7,23 @@ const categoryServices = {
     const data = await callAPI.get(`category/list?${searchOptions}`);
     return data;
   },
-  deleteCategory: async (id) => {
+  deleteCategory: async (id: string) => {
     return await callAPI.delete(`category/remove/${id}`);
   },
   getCategoryByTitle: async (title = "") => {
     const data = await callAPI.get(`category/${title}`);
     return data;
   },
-  updateCategory: async ({ id, data }) => {
+  updateCategory: async ({
+    id,
+    data,
+  }: {
+    id: string;
+    data: Omit<Category, "slug" | "icon">;
+  }) => {
     return await callAPI.patch(`category/update/${id}`, data);
   },
-  createCategory: async (data) => {
+  createCategory: async (data: Omit<Category, "slug" | "icon">) => {
     return await callAPI.post("category/add", data);
   },
 };
