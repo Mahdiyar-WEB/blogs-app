0b2c6b7192939d8b0d7877bf32c345fe050e4302
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-12 17:51:25 +0330

    update commentServices api

--- api/commentServices.ts
+++ api/commentServices.ts
@@ -1,13 +1,17 @@
 import callAPI from "api/callAPI";
 
 const commentServices = {
-  addNewComment: async (values) => {
+  addNewComment: async (values: {
+    text: string;
+    parentId: string;
+    postId: string;
+  }) => {
     return await callAPI.post("comment/add", values);
   },
-  getAllComments: async (cookies, searchOption) => {
+  getAllComments: async (cookies: string, searchOption: string) => {
     return await callAPI.get(`comment/list?${searchOption}`, cookies);
   },
-  deleteComment: async (id) => {
+  deleteComment: async (id: string) => {
     return await callAPI.delete(`comment/remove/${id}`);
   },
 };
