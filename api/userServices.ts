6918d5f0b4eaac5608bef0988bdd760f2ef942de
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-12 17:51:48 +0330

    update userServices api

--- api/userServices.ts
+++ api/userServices.ts
@@ -1,16 +1,16 @@
 import callAPI from "api/callAPI";
 
 const userServices = {
-  getAllUsers: async (cookies, searchOptions = "") => {
+  getAllUsers: async (cookies: string, searchOptions = "") => {
     return await callAPI.get(`user/list?${searchOptions}`, cookies);
   },
-  deleteUser: async (id) => {
+  deleteUser: async (id: string) => {
     return await callAPI.delete(`user/remove/${id}`);
   },
-  getUserById: async (cookies, id = "") => {
+  getUserById: async (cookies: string, id = "") => {
     return await callAPI.get(`user/profile/${id}`, cookies);
   },
-  updateUser: async ({ id, data }) => {
+  updateUser: async ({ id, data }: { id: string; data: FormData }) => {
     return await callAPI.patch(`user/update/${id}`, data);
   },
 };
