ebe6591b7bcf9c55ab8364f6cf6c0b9b51518cb5
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-12 16:55:56 +0330

    add TS to category hooks

--- hooks/categories/useGetCategories.ts
+++ hooks/categories/useGetCategories.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import categoryServices from "api/categoryServices";
+import { Category } from "lib/models/Category";
 
-const useGetCategories = (params) => {
+const useGetCategories = (params: string) => {
   const { data, isLoading } = useQuery({
     queryFn: () => {
       return categoryServices.getAllCategories(params);
@@ -10,9 +11,11 @@ const useGetCategories = (params) => {
   });
 
   let options = data?.data?.categories
-    ? data?.data?.categories.map(({ _id, title }) => {
-        return { label: title, value: _id };
-      })
+    ? data?.data?.categories.map(
+        ({ _id, title }: Category & { _id: string }) => {
+          return { label: title, value: _id };
+        },
+      )
     : [];
 
   return {
