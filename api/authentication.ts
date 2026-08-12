fb7760ac3002e6325d556f784aa7984b53f8e5b3
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-12 17:50:51 +0330

    update authentication api

--- api/authentication.ts
+++ api/authentication.ts
@@ -1,18 +1,19 @@
+import { SigninInputs, SignupInputs } from "types/authentication/auth";
 import callAPI from "./callAPI";
 
 const authentication = {
-  signUp: async (inputs) => {
+  signUp: async (inputs: SignupInputs) => {
     return await callAPI.post("user/signup", inputs).then((data) => data);
   },
-  signIn: async (inputs) => {
+  signIn: async (inputs: SigninInputs) => {
     return await callAPI.post("user/signin", inputs).then((data) => data);
   },
-  getUser: async (cookies) => {
+  getUser: async (cookies?: string) => {
     const res = await callAPI.get("user/profile", cookies);
     return res;
   },
   logout: async () => {
-    return await callAPI.post("user/logout").then((data) => data);
+    return await callAPI.post("user/logout", {}).then((data) => data);
   },
 };
 
